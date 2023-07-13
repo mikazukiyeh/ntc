08bc9361b29d9aeeee74f28d154f1daa25f9e6f7
Luna Yeh <131629497+mikazukiyeh@users.noreply.github.com>
2023-07-13 11:58:19 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,10 @@
-import json
-from flask import Flask, request, Response
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
-def compare():
-        a = int(request.args.get("a"))
-        b = int(request.args.get("b"))
-
-        try:
-            if a > b:
-                return Response(json.dumps({"status":"a>b"}), mimetype="application/json")
-            else:
-                return Response(json.dumps({"status":"a<=b"}), mimetype="application/json")
-        except:
-            return Response(json.dumps({"status":"invalid parameter"}), status = 403, mimetype="application/json")
+def home():
+    return "OK"
 
 if __name__ == "__main__":
     app.debug = False
